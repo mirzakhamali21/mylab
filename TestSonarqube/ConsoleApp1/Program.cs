558866754dd1aaf0156c490e333b1d5028f8c035
mirzakhamali21 <56858675+mirzakhamali21@users.noreply.github.com>
2019-10-22 13:36:48 +0700

    ok
    commit

---
+++ TestSonarqube/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program conprogram = new Program();
+            conprogram.getid("1");
+        }
+
+        public string getid(string id)
+        {
+            var connection = new SqlConnection();
+            try
+            {
+                connection.ConnectionString = "test db";
+                connection.Open();
+                var selectsql = string.Format("select * from test where id='{0}';", id);
+                var selectcommand = new SqlCommand(selectsql, connection);
+                var datareader = selectcommand.ExecuteReader();
+                return datareader.GetString(0);
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+                
+            }
+            
+        }
+    }
+}
